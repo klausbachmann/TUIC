c12c6b7283b3a885fd64d335941dbbb229e4fefc
Maddin <martin@derwolters.de>
2017-06-02 15:14:31 +0200

    neu

--- src/main/java/TUISelenium/tuic/ibe/tests/BaseTest.java
+++ src/main/java/TUISelenium/tuic/ibe/tests/BaseTest.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -15,6 +16,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptEngine;
+
 public class BaseTest {
 
 	WebDriver driver;
@@ -24,18 +27,16 @@ public class BaseTest {
 	public void init(String browser) throws MalformedURLException {
 
 		DesiredCapabilities caps;
-		
+
 		switch (browser) {
 		case "chrome":
-			//System.setProperty("webdriver.chrome.driver", "chromedriver");
-			//driver = new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+			driver = new ChromeDriver();
 
-			// System.setProperty("phantomjs.binary.path", "phantomjs");
-			// driver = new PhantomJSDriver();
-
-			caps = new DesiredCapabilities().chrome();
-			caps.setVersion("58");
-			driver = new RemoteWebDriver(new URL("http://10.234.253.101:4444/wd/hub"), caps);
+			// caps = new DesiredCapabilities().chrome();
+			// caps.setVersion("58");
+			// driver = new RemoteWebDriver(new
+			// URL("http://10.234.253.101:4444/wd/hub"), caps);
 			driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 
@@ -49,11 +50,21 @@ public class BaseTest {
 			driver.manage().window().maximize();
 			break;
 
+		case "phantom":
+			System.setProperty("phantomjs.binary.path", "phantomjs");
+			driver = new PhantomJSDriver();
+			break;
+
 		default:
 			break;
 		}
-		System.out.println(browser);
-
+		System.out.println("Starting Test with browser: " + browser);
+	}
+	
+	public String getLocalStorage(String key){
+		JavascriptExecutor js = ((JavascriptExecutor)driver);
+		String s = (String) js.executeScript("return window.localStorage.getItem('"+ key + "')");
+		return s;
 	}
 
 	@AfterTest
